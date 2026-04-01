cf59e397b42b8874763f54d7a0c1b8b00ecf86cd
zaitokuzzz <thebluecat3377@gmail.com>
2026-04-01 19:34:52 +0700

    Update __init__ model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,13 @@ from app.models.memory_item import MemoryItem
 from app.models.memory_embedding import MemoryEmbedding
 from app.models.daily_summary import DailySummary
 from app.models.context_bundle import ContextBundle
+from app.models.retrieval_query import RetrievalQuery
+from app.models.retrieval_result import RetrievalResult
+from app.models.retrieval_feedback import RetrievalFeedback
+from app.models.browser_source import BrowserSource
+from app.models.semantic_cluster import SemanticCluster
+from app.models.memory_archive import MemoryArchive
+from app.models.worker_job import WorkerJob
 
 __all__ = [
     "Owner",
@@ -12,4 +19,11 @@ __all__ = [
     "MemoryEmbedding",
     "DailySummary",
     "ContextBundle",
+    "RetrievalQuery",
+    "RetrievalResult",
+    "RetrievalFeedback",
+    "BrowserSource",
+    "SemanticCluster",
+    "MemoryArchive",
+    "WorkerJob",
 ]
